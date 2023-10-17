c3a6401a62c892ea76af5bcaaa54b4dc3a35eb05
Ai Chain <hitthumit@gmail.com>
2023-10-18 01:19:38 +0800

    快速迭代 >> 日拱一卒

--- src/background/types.ts
+++ src/background/types.ts
@@ -27,5 +27,4 @@ generateAnswer is the name of the method.
 (params: GenerateAnswerParams) describes the parameter that generateAnswer accepts. The GenerateAnswerParams is likely another interface or type defined elsewhere in the codebase.
 
 Promise<{ cleanup?: () => void }> indicates that the method returns a Promise. The resolved value of the promise is an object with an optional property cleanup that is a function taking no arguments and returning no value.
-
 */
