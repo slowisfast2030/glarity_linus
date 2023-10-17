3def66491db5810e7e16c2f9639e193a25e3d29b
Ai Chain <hitthumit@gmail.com>
2023-10-18 01:19:15 +0800

    快速迭代 >> 日拱一卒

--- src/background/types.ts
+++ src/background/types.ts
@@ -18,3 +18,14 @@ export interface GenerateAnswerParams {
 export interface Provider {
   generateAnswer(params: GenerateAnswerParams): Promise<{ cleanup?: () => void }>
 }
+/*
+from chatpgt:
+generateAnswer(params: GenerateAnswerParams): Promise<{ cleanup?: () => void }>: This is the method declaration within the Provider interface. It specifies the signature and return type of the method.
+
+generateAnswer is the name of the method.
+
+(params: GenerateAnswerParams) describes the parameter that generateAnswer accepts. The GenerateAnswerParams is likely another interface or type defined elsewhere in the codebase.
+
+Promise<{ cleanup?: () => void }> indicates that the method returns a Promise. The resolved value of the promise is an object with an optional property cleanup that is a function taking no arguments and returning no value.
+
+*/
